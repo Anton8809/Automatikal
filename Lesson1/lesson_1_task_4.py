9e773597360dc0a61d7c6b4c452e52d9aba2e606
Антон Исаев <Melkiy206.6@yandex.ru>
2024-03-11 21:52:30 +0700

    add first homework

--- Lesson1/lesson_1_task_4.py
+++ Lesson1/lesson_1_task_4.py
@@ -1,5 +1,3 @@
 first_name = input ("Как вас зовут? ")
 last_name = input ("Какая у вас фамилия")
-first_name = "Наташа"
-last_name = "Исаева"
-print ("Вас зовут ", last_name, first_name)
+print("Вас зовут ", last_name, first_name)
